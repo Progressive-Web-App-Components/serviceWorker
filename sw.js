255719fde77176f70c257d4154bd99c7cc54aebe
Jagat Jeevan Sahoo <jagatjeevan@users.noreply.github.com>
2018-07-18 15:35:58 +0530

    update path for right cache-ing

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@ var CacheArray = [
   'https://progressive-web-app-components.github.io/serviceWorker/offline.js',
   'https://progressive-web-app-components.github.io/serviceWorker/style.css',
 ];
-var offlineHtml = 'https://jagatjeevan.github.io/serviceWorkerSpike/offline.html';
+var offlineHtml = 'https://progressive-web-app-components.github.io/serviceWorker/offline.html';
 
 self.addEventListener('install', function(event) {
   console.log('SW installed ', version , ' -> ', new Date().toLocaleString());
